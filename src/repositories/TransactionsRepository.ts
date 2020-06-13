d60b21c999c483512409c3fc41ca01c1a902ffaf
wilfigueredo <william_figueredo@hotmail.com>
2020-06-12 21:36:29 -0300

    finalizado desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,17 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +22,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter((transaction) => transaction.type === 'income')
+      .reduce((total, transactionReduce) => { return total += transactionReduce.value }, 0);
+
+    const outcome = this.transactions
+      .filter((transaction) => transaction.type === 'outcome')
+      .reduce((total, transactionReduce) => { return total += transactionReduce.value }, 0);
+
+    console.log(outcome);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
